bfd0f7a154cc44ec8ef838fcbb48a4fa7c25fe80
Vestionz <159621234+vestionz@users.noreply.github.com>
2025-12-26 21:38:02 +0700

    Create languages.js

---
+++ src/data/languages.js
@@ -0,0 +1,20 @@
+export const LANGUAGES = [
+  {
+    id: "javascript",
+    name: "JavaScript",
+    entry: "index.js",
+    template: "console.log('Hello JavaScript');"
+  },
+  {
+    id: "python",
+    name: "Python",
+    entry: "main.py",
+    template: "print('Hello Python')"
+  },
+  {
+    id: "cpp",
+    name: "C++",
+    entry: "main.cpp",
+    template: "#include <iostream>\nint main(){ std::cout << \"Hello\"; }"
+  }
+];
