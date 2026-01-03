11eb70607b8280fc2f07ef13c07d1b4078ee1d0e
Vestionz <159621234+vestionz@users.noreply.github.com>
2026-01-04 02:42:59 +0700

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -66,7 +66,7 @@ export default function Footer() {
       <footer className="footer">
         <a
           href="/socials"
-          className={`footer-item ${path === "/project" ? "active" : ""}`}
+          className={`footer-item ${path === "/socials" ? "active" : ""}`}
         >
           <FaUsers />
           <span>Socials</span>
